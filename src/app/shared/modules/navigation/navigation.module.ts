c0968c97a15718db25b96149378f85091ef3a925
Nathan Blankson <nathanblankson@nathans-mbp.home>
2020-05-10 16:29:56 +0100

    Addeding routing

--- src/app/shared/modules/navigation/navigation.module.ts
+++ src/app/shared/modules/navigation/navigation.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NavigationWrapperComponent } from './navigation-wrapper/navigation-wrapper.component';
 import { NavListComponent } from './nav-list/nav-list.component';
@@ -7,7 +8,7 @@ import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [NavigationWrapperComponent, NavListComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   exports: [NavigationWrapperComponent],
 })
 export class NavigationModule {}
