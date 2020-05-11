fcbee96c05d66b5711f252836726b8f680f2b955
Nathan Blankson <nathanblankson@nathans-mbp.home>
2020-05-11 11:06:58 +0100

    Added daily card styling

--- src/app/+forecast/components/daily-card/daily-card.component.ts
+++ src/app/+forecast/components/daily-card/daily-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-daily-card',
   templateUrl: './daily-card.component.html',
-  styleUrls: ['./daily-card.component.scss']
+  styleUrls: ['./daily-card.component.scss'],
 })
 export class DailyCardComponent implements OnInit {
+  @Input()
+  active: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onClick() {
+    console.log('card clicked');
   }
-
 }
