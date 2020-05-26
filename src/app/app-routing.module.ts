fdebe76363c2cb47b8e5503834115daa0dd03a2b
Nathan Blankson <nathanblankson@nathans-mbp.home>
2020-05-26 17:06:33 +0100

    Generate login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'forecast',
-    loadChildren: './+forecast/forecast.module#ForecastModule',
-  },
-  {
-    path: 'favourites',
-    loadChildren: './+favourites/favourites.module#FavouritesModule',
-  },
-  {
-    path: 'settings',
-    loadChildren: './+settings/settings.module#SettingsModule',
-  },
-  {
-    path: '',
-    redirectTo: 'forecast',
-    pathMatch: 'full',
-  },
+    {
+        path: 'login',
+        loadChildren: './+login/login.module#LoginModule'
+    },
+    {
+        path: 'forecast',
+        loadChildren: './+forecast/forecast.module#ForecastModule',
+    },
+    {
+        path: 'favourites',
+        loadChildren: './+favourites/favourites.module#FavouritesModule',
+    },
+    {
+        path: 'settings',
+        loadChildren: './+settings/settings.module#SettingsModule',
+    },
+    {
+        path: '',
+        redirectTo: 'forecast',
+        pathMatch: 'full',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
