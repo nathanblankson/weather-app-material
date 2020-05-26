a3b79533229571aed6d3d51377b2057f5e0f3584
Nathan Blankson <nathanblankson@nathans-mbp.home>
2020-05-26 16:16:24 +0100

    ngxs to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appState } from '@store/index';
 import { MaterialModule } from '@shared/modules/material/material.module';
 import { NavigationModule } from '@shared/modules/navigation/navigation.module';
 import { ForecastModule } from '@forcast/forecast.module';
@@ -17,7 +20,9 @@ import { ForecastModule } from '@forcast/forecast.module';
         MaterialModule,
         NavigationModule,
         ForecastModule,
-        HttpClientModule
+        HttpClientModule,
+        NgxsModule.forRoot(appState),
+        NgxsReduxDevtoolsPluginModule.forRoot()
     ],
     bootstrap: [AppComponent],
 })
