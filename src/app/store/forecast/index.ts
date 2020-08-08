406552d0dba79b67481b9a5a93072df736bc7061
Nathan Blankson <nathan@sourcebreaker.com>
2020-08-08 17:56:01 +0100

    reordering

--- src/app/store/forecast/index.ts
+++ src/app/store/forecast/index.ts
@@ -1,4 +1,4 @@
 export * from './forecast.actions';
-export * from './forecast.selectors'
+export * from './forecast.selectors';
 export * from './forecast.state';
 export * from './forecast-state.model';
